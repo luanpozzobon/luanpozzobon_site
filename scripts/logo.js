6cad5c6e8f1d9b31af1fc4f3181e81ee9fdca450
LuanPozzobon <108753073+LuanPozzobon@users.noreply.github.com>
2023-12-10 12:20:02 -0300

    Initial Commit

---
+++ scripts/logo.js
@@ -0,0 +1,9 @@
+const image = document.getElementById("logo");
+
+function originalLogo() {
+    image.src = "/luanpozzobon_site/assets/images/logo-original.png";
+}
+
+function whiteLogo() {
+    image.src = "/luanpozzobon_site/assets/images/logo-white.png";
+}
