e58ea93156e05c19f00ef29fac92d93accc8c5d4
LuanPozzobon <108753073+LuanPozzobon@users.noreply.github.com>
2023-12-24 13:31:19 -0300

    Projects Page

---
+++ pages/projects/moonvs/scripts/showcontent.js
@@ -0,0 +1,38 @@
+const project = document.getElementById('content').innerHTML;
+
+const tabs = document.getElementById('tabs').children;
+const content = document.getElementById('content');
+
+function select(element) {
+    unselect();
+
+    var id = element.id;
+
+    element.classList.add('selected');
+
+    if (id === 'project'){
+        content.innerHTML = project;
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `/luanpozzobon_site/pages/projects/moonvs/templates/${id}.html`, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(xhr.responseText, 'text/html');
+
+            content.innerHTML = doc.getElementById('template').innerHTML;
+        }
+    };
+
+    xhr.send();
+}
+
+function unselect() {
+    Array.from(tabs).forEach(e => {
+        e.classList.add('unselected');
+        e.classList.remove('selected');
+    })
+}
